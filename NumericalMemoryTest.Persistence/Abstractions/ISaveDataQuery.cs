ec496bc06e6419d51b87b39d4802f8a361370b7e
Tom Layzell <tom.layzell@hotmail.co.uk>
2019-12-29 13:19:58 +0000

    Database retrevial code

---
+++ NumericalMemoryTest.Persistence/Abstractions/ISaveDataQuery.cs
@@ -0,0 +1,12 @@
+﻿using NumericalMemoryTest.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumericalMemoryTest.Persistence.Abstractions
+{
+    public interface ISaveDataQuery
+    {
+        bool SaveData(Result result);
+    }
+}
--- NumericalMemoryTest.Persistence/Queries/AddDataQuery.cs
+++ NumericalMemoryTest.Persistence/Queries/AddDataQuery.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using NumericalMemoryTest.Infrastructure.Models;
+using NumericalMemoryTest.Persistence.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace NumericalMemoryTest.Persistence.Queries
 {
-    class AddDataQuery
+    class AddDataQuery : ISaveDataQuery
     {
+        private readonly NumericalMemoryContext _context;
+        public AddDataQuery(NumericalMemoryContext context)
+        {
+            _context = context;
+        }
+
+        public bool SaveData(Result result)
+        {
+            try
+            {
+                using (var conn = _context.GetConnection())
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand($"select * from Result where id = {UserId}", conn);//TODO: Sql command
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
